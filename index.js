de5f0564892981bf3f3b1bda4e3271eee5ed52be
Josh Mabry <mabry1985@gmail.com>
2019-12-02 09:40:49 -0800

    add functionality for scraping titles from search results

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+
+const scrapingResults = [
+  {
+    title: "",
+    datePosted: new Date("2019-07-25 12:34:00"),
+    neighborhood: "(palo alto)",
+    url: "https://portland.craigslist.org/mlt/sof/d/vancouver-entry-level-python-yaml/7028441064.html",
+    jobDescription: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, voluptatum.",
+    compensation: "Up to $5.00 per hour"
+  }
+]
 
 async function main() {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
-  await page.goto("https://www.google.com");
+  await page.goto("https://portland.craigslist.org/d/software-qa-dba-etc/search/sof");
+
+  const html = await page.content();
+  const $ = cheerio.load(html);
+
+  $(".result-title").each((index, element) => console.log($(element).text()))
+
 
 }
 
